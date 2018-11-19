c3d02b02b2e43852325943fc874fd58e17afd871
Emmett <ehaysle@siue.edu>
2018-11-19 00:03:11 -0600

    major changes

--- vibrant.py
+++ vibrant.py
@@ -1,94 +1,162 @@
 import re
+from ascii_seq import *
 
+#Color convience methods
+def red(text):
+  return partial(fred,text)
 
-fdefault = '\033[39m'
-fblack = '\033[30m'
-fred = '\033[31m'
-fgreen = '\033[32m'
-forange = '\033[33m'
-fblue = '\033[34m'
-fmegenta = '\033[35m'
-fcyan = '\033[36m'
-flgray = '\033[37m'
-fdgray = '\033[90m'
-flred = '\033[91m'
-flgreen = '\033[92m'
-flyellow = '\033[93m'
-flblue = '\033[94m'
-flmagenta = '\033[95'
-flcyan = '\033[96m'
-fwhite = '\033[97m'
+def black(text):
+  return partial(fblack,text)
+
+def green(text):
+  return partial(fgreen, text)
+
+def orange(text):
+  return partial(forange, text)
+
+def blue(text):
+  return partial(fblue, text)
+
+def magenta(text):
+  return partial(fmegenta, text)
+
+def cyan(text):
+  return partial(fcyan, text)
+
+def lgray(text):
+  return partial(lgray, text)
+
+def dgray(text):
+  return partial(fdgray, text)
+
+def lred(text):
+  return partial(flred, text)
+
+def lgreen(text):
+  return partial(flgreen, text)
+
+def lyellow(text):
+  return partial(flyellow, text)
+
+def lblue(text):
+  return partial(flblue, text)
+
+def lmagenta(text):
+  return partial(flmagenta, text)
+
+def lcyan(text):
+  return partial(flcyan, text)
+
+def white(text):
+  return partial(fwhite, text)
+#End font color convience method
 
 #Background Color
-default = '\033[49m'
-black = '\033[40m'
-red = '\033[41m'
-green = '\033[42m'
-yellow = '\033[43'
-blue = '\033[44m'
-magenta = '\033[45m'
-cyan = '\033[46m'
-lgray = '\033[47m'
-dgray = '\033[100m'
-lred = '\033[101m'
-lgreen = '\033[102m'
-lyellow = '\033[103m'
-lblue = '\033[104m'
-lmagenta = '\033[105m'
-lcyan = '\033[106m'
-white = '\033[107m'
+def _black(text):
+  return partial(bblack, text)
 
-#Symbols
-_warning = '[!] '
-_question = '[?] '
-_error = '[-] '
-_add = '[+] '
-_check = '[✓] '
+def _red(text):
+  return partial(bred, text)
 
-bold = '\033[1m'
-dim = '\033[2m'
-underline = '\033[4m'
-reverse = '\033[7m'
-hidden = '\033[8m'
-reset = '\033[0m'
+def _green(text):
+  return partial(bgreen, text)
 
+def _yellow(text):
+  return partial(byellow, text)
+
+def _blue(text):
+  return partial(bblue, text)
+
+def _magenta(text):
+  return partial(bmagenta, text)
+
+def _cyan(text):
+  return partial(bcyan, text)
+
+def _lgray(text):
+  return partial(blgray, text)
+
+def _dgray(text):
+  return partial(bdgray, text)
+
+def _lred(text):
+  return partial(blred, text)
+
+def _lgreen(text):
+  return partial(blgreen, text)
+
+def _lyellow(text):
+  return partial(blyellow, text)
+
+def _lblue(text):
+  return partial(blblue, text)
+
+def _lmagenta(text):
+  return partial(blmagenta, text)
+
+def _lcyan(text):
+  return partial(blcyan, text)
+
+def _white(text):
+  return partial(bwhite, text)
+#End background colors
+
+#Style convience methods
+def bold(text):
+  return bold_ + text + reset_
+
+def dim(text):
+  return dim_ + text + reset_
+
+def underline(text):
+  return underline_ + text + reset_
+
+def invert(text):
+  return reverse_ + text + reset_
+
+def hidden(text):
+  return hidden_ + text + reset_
+#End Style Convience methods
+
+#Symbol convience methods
 def esc_num(sequence):
   first_split = re.search(r'\[\d+', sequence).group(0)
   return first_split.split('[')[1]
 
 def warning(text = "", _full = False):
-  result = full(forange, _warning,text) if _full else \
-  partial(forange, _warning) + text + reset
+  result = full(forange, warning_,text) if _full else \
+  partial(forange, warning_) + text + reset_
   return result
 
 def error( text = "", _full = False):
-  result = full(fred, _error, text) if _full else \
-  partial(fred, _error) + text + reset
+  result = full(fred, error_, text) if _full else \
+  partial(fred, error_) + text + reset_
   return result
 
 def question( text = "", _full = False):
-  result = full(fcyan, _question, text) if _full else \
-  partial(fcyan, _question) + text + reset
+  result = full(fcyan, question_, text) if _full else \
+  partial(fcyan, question_) + text + reset_
   return result
 
 def add( text = "", _full = False):
-  result = full(fgreen, _add, text) if _full else \
-  partial(fgreen, _add) + text + reset
+  result = full(fgreen, add_, text) if _full else \
+  partial(fgreen, add_) + text + reset_
   return result
 
 def check( text = "", _full = False):
-  result = full(fmegenta, _check, text) if _full else \
-  partial(fmegenta,_check) + text + reset
+  result = full(fmegenta, check_, text) if _full else \
+  partial(fmegenta,check_) + text + reset_
   return result
+#End Symbol Convience methods
 
 def full(color,symbol, text):
-  return color + symbol + text + reset
+  return color + symbol + text + reset_
 
 def partial(color,symbol):
-  return color + symbol + reset
+  return color + symbol + reset_
 
-def custom(fcolor, text, bcolor = default, fmt = default):
+def custom(fcolor, text, bcolor = bdefault, fmt = bdefault):
   f_num = esc_num(fcolor)
   fmt_num = esc_num(fmt)
   b_num = esc_num(bcolor)
-  return "\033[" + f_num + ";" + fmt_num + ";" + b_num + "m" + text + reset
+  return "\033[" + f_num + ";" + fmt_num + ";" + b_num + "m" + text + reset_
